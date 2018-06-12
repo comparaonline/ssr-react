d78d934fcc56359d5b197c740140c0961d5749fb
Guillermo Puente Sandoval <guillermo.ps09@gmail.com>
2018-06-12 17:46:27 -0400

    Added extended config

--- config/index.js
+++ config/index.js
@@ -2,21 +2,29 @@ const merge = require('lodash/merge');
 const _get = require('lodash/get');
 const _has = require('lodash/has');
 
+const extendObject = require('./extend').default;
+
 const defaultConfig = require('./default.json');
 
 const buildConfig = (env = process.env.NODE_ENV || 'development') => {
   const envConfig = require(`./${env}.json`);
   const config = merge({}, defaultConfig, envConfig);
 
-  return {
+  const baseConfigObject = {
     get: (path, defaultValue) => _get(config, path, defaultValue),
-    has: (path) => _has(config, path),
+    has: path => _has(config, path),
     getFullConfig: () => config,
   };
+
+  const finalConfig = merge({}, baseConfigObject, {
+    extended: extendObject(config),
+  });
+
+  return finalConfig;
 };
 
 const configObject = buildConfig();
-const { get, has, getFullConfig } = configObject;
+const { get, has, getFullConfig, extended } = configObject;
 
-export { get, has, getFullConfig };
+export { get, has, getFullConfig, extended };
 export default configObject;
