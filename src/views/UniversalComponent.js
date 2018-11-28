aaf302f7e2e4b99765feb9c08d7cc1b6f916aa61
Guillermo Puente Sandoval <guillermo.ps09@gmail.com>
2018-11-28 15:43:26 -0300

    Changed minDelay

--- src/views/UniversalComponent.js
+++ src/views/UniversalComponent.js
@@ -3,6 +3,6 @@ import universal from 'react-universal-component';
 
 const determineHowToLoad = ({ page }) => typeof page !== 'string' ? () => page() : import(`./${page}`)
 
-const UniversalComponent = universal(determineHowToLoad, { minDelay: 200 });
+const UniversalComponent = universal(determineHowToLoad, { minDelay: 800 });
 
 export default UniversalComponent;
