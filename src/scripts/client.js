fd555aa36b8b3c57a3b2f34641b0641461aa4a28
Guillermo Puente Sandoval <guillermo.ps09@gmail.com>
2018-12-04 11:34:28 -0300

    Setup webpack-dev-server for support HMR and SPA mode

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -3,13 +3,17 @@ const clientConfig = require('../../webpack/client')();
 const path = require('path');
 const WebpackDevServer = require('webpack-dev-server');
 
-const compiler = webpack(clientConfig);
-
-const devServer = new WebpackDevServer(compiler, {
+const options = {
   contentBase: path.join(__dirname, '../../dist'),
   hot: true,
-});
+  historyApiFallback: true,
+  host: 'localhost',
+};
 
-devServer.listen(3011, '0.0.0.0', () => {
+WebpackDevServer.addDevServerEntrypoints(clientConfig, options);
+const compiler = webpack(clientConfig);
+const devServer = new WebpackDevServer(compiler, options);
+
+devServer.listen(3011, 'localhost', () => {
   console.log('starting development server...');
 });
